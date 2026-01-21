16bee788eda26aa81846e71a99045505e27a8d39
Ankit-chauhan00 <ankitchauhan820066@gmail.com>
2026-01-21 11:28:56 +0530

    done with tailwind and pretirre, metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+
 const Home = () => {
-  return <div className="bg-amber-50 text-red-400">Home</div>;
-};
-export default Home;
+  return (
+    <div className='bg-red-500 text-9xl text-white text-center  top-1/2 font-inter'>Home</div>
+  )
+}
+
+export default Home
