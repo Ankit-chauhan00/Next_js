eb591c9fdec6c5f66782f0d1dc702232927e1c48
Ankit-chauhan00 <ankitchauhan820066@gmail.com>
2026-04-16 15:37:07 +0530

    implemented Account bu provider Route

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,31 @@
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-error";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema} from "@/lib/validation";
+import { APIErrorResponse } from "@/types/global";
+import { NextResponse } from "next/server";
+import z from "zod";
+
+export async function POST(request: Request){
+    const { providerAccountId } = await request.json();
+    try {
+        await dbConnect();
+        const validatedData = AccountSchema.partial().safeParse({providerAccountId});
+
+        if(!validatedData.success){
+            const formattedError = z.treeifyError(validatedData.error);
+            throw new ValidationError(formattedError);
+        }
+
+        const account  = await Account.findOne({ providerAccountId });
+
+        if(!account) throw new NotFoundError('Account');
+
+        return NextResponse.json({success: true, data: account}, {status: 200});
+    }
+
+     catch (error) {
+        return handleError(error,'api') as APIErrorResponse;
+    }
+}
