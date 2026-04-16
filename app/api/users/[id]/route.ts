f13658b173abd22251694a4f8d7354d0239c94f5
Ankit-chauhan00 <ankitchauhan820066@gmail.com>
2026-04-16 15:02:01 +0530

    implemented dynamic account route

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,10 +1,11 @@
-import User from "@/database/user.model";
+import Account from "@/database/account.model";
 import handleError from "@/lib/handlers/error";
-import { NotFoundError } from "@/lib/http-error";
+import { NotFoundError, ValidationError } from "@/lib/http-error";
 import dbConnect from "@/lib/mongoose";
-import { UserSchema } from "@/lib/validation";
+import { AccountSchema } from "@/lib/validation";
 import { APIErrorResponse } from "@/types/global";
 import { NextResponse } from "next/server";
+import z from "zod";
 
 
 // GET /api/users/[id]
@@ -12,14 +13,15 @@ export async function GET(request : Request, {params } : { params: Promise<{id: 
 
     const {id} = await params;
 
-    if(!id) throw new NotFoundError('User');
+    if(!id) throw new NotFoundError('Account');
 
     try {
+        await dbConnect();
 
-        const user = await User.findById(id);
-        if(!user) throw new NotFoundError("User");
+        const account = await Account.findById(id);
+        if(!account) throw new NotFoundError("Account");
 
-        return NextResponse.json({success: true, data: user}, {status: 200});
+        return NextResponse.json({success: true, data: account}, {status: 200});
 
 
     } catch (error) {
@@ -27,20 +29,20 @@ export async function GET(request : Request, {params } : { params: Promise<{id: 
     }
 }
 
-// DELETE /api/users/id
+// DELETE /api/users/[id]
 export async function DELETE(request : Request, {params} : {params: Promise<{id: string}>}){
 
     const {id} = await params;
-    if(!id) throw new NotFoundError('User');
+    if(!id) throw new NotFoundError('Account');
 
     try {
         await dbConnect();
 
-        const user = await User.findByIdAndDelete(id);
+        const account = await Account.findByIdAndDelete(id);
 
-        if(!user) throw new NotFoundError("User");
+        if(!account) throw new NotFoundError("Account");
 
-        return NextResponse.json({success: true, data: user}, {status: 204});
+        return NextResponse.json({success: true, data: account}, {status: 200});
 
     } catch (error) {
         return handleError(error, "api") as APIErrorResponse;
@@ -51,19 +53,24 @@ export async function DELETE(request : Request, {params} : {params: Promise<{id:
 export async function PUT(request : Request, {params}: {params: Promise<{id: string}>}){
 
     const { id } = await params;
-    if(!id) throw new NotFoundError("User");
+    if(!id) throw new NotFoundError("Account");
 
     try {
         await dbConnect();
 
         const body = await request.json();
-        const validatedData = UserSchema.partial().parse(body);
+        const validatedData = AccountSchema.partial().safeParse(body);
 
-        const updatedUser = await User.findByIdAndUpdate(id, validatedData,{ new : true});
+        if(!validatedData.success) {
+         const formattedError = z.treeifyError(validatedData.error);
+         throw new ValidationError(formattedError);
+        }
 
-        if(!updatedUser) throw new NotFoundError("User");
+        const updatedAccount = await Account.findByIdAndUpdate(id, validatedData.data,{ new : true});
 
-        return NextResponse.json({ success: true, data: updatedUser}, {status: 200});
+        if(!updatedAccount) throw new NotFoundError("Account");
+
+        return NextResponse.json({ success: true, data: updatedAccount}, {status: 200});
 
     } catch (error) {
         return handleError(error,'api') as APIErrorResponse;
