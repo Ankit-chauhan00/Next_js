66f6039033e95522d3487627fe410371f76795d0
Ankit-chauhan00 <ankitchauhan820066@gmail.com>
2026-06-01 15:44:08 +0530

    api roues fixing

--- lib/api.ts
+++ lib/api.ts
@@ -11,7 +11,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3
 export const api = {
   auth: {
     oAuthSignIn: ({ user, provider, providerAccountId }: SignInWithOAuthParams) =>
-      fetchHandler(`${API_BASE_URL}/auth/${ROUTES.SIGN_IN_WITH_OAUTH}`, {
+      fetchHandler(`${API_BASE_URL}/api/auth/${ROUTES.SIGN_IN_WITH_OAUTH}`, {
         method: "POST",
         body: JSON.stringify({ user, provider, providerAccountId }),
       }),
